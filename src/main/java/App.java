ef320a81e1545e25d845c37916397069674b6869
JuanR <ramirezjuan9806@gmail.com>
2022-07-15 22:51:36 -0500

    Estableciendo conexion a base de datos

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,9 @@
+import utilities.ConectionToSQL;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Funciona!");
+        ConectionToSQL con= new ConectionToSQL();
+        con.connectionSql();
     }
 }
---
+++ src/main/java/utilities/ConectionToSQL.java
@@ -0,0 +1,22 @@
+package utilities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class ConectionToSQL {
+    Connection conection=null;
+    MessagesWithJboss message=new MessagesWithJboss();
+    public Connection connectionSql(){
+        try{
+            conection = DriverManager.getConnection("jdbc:mysql://localhost:3325/laboratorio_java","root","");
+            Statement statement = conection.createStatement();
+            message.printMessage("Conexión exitosa!");
+
+        }catch (Exception e){
+            message.printMessage("No se pudo establecer la conexión a la base de datos!");
+            message.printMessage(e.getMessage());
+        }
+        return conection;
+    }
+}
